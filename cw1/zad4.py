0d3d08bd462f90c49e9fceb732d03e8e7dc35cae
jan.bylina <jan.bylina@skni.umcs.pl>
2023-03-16 23:10:50 +0100

    change f name

--- cw1/zad4.py
+++ cw1/zad4.py
@@ -1,13 +1,13 @@
 import socket
 
 
-def get_ip_address(ip_address: str):
+def get_name_for_ip_adress(ip_address: str):
     return socket.gethostbyaddr(ip_address)
 
 
 def main():
     ip_address = input("Podaj adres ip: ")
-    print(get_ip_address(ip_address))
+    print(get_name_for_ip_adress(ip_address))
 
 
 if __name__ == "__main__":
